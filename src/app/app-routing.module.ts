4dd2c34321adb89137fc542c0ee1bdaa47926016
saiannamaiah <annamaiahsai@gmail.com>
2023-09-20 15:26:56 +0530

    Ecomm Project Setup Files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { AuthGuardGuard } from './auth-guard.guard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, },
+  { path: 'seller-auth', component: SellerAuthComponent, },
+  { path: 'seller-home', component: SellerHomeComponent, canActivate:[AuthGuardGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/auth-guard.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SellerService } from './services/seller.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+  constructor(private sellerServices:SellerService) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.sellerServices.isSellerLoggedIn;
+  }
+  
+}
